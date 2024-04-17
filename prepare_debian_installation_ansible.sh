7be0a6074dce7965a543f5253929895cc65e80ef
David Rotermund <davrot@neuro.uni-bremen.de>
2024-04-17 23:02:51 +0200

    Update

--- prepare_debian_installation_ansible.sh
+++ prepare_debian_installation_ansible.sh
@@ -2,6 +2,7 @@ apt-get update
 apt install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible
+apt install gh
 
-apt install mc vi sshpass ssh-keygen inetutils-ping
+apt install mc vi sshpass ssh-keygen inetutils-ping git-all
 
